7e046dc8e9991eb4c66bb4beaec68452f42de842
laurapessine <laurapessine@outlook.com>
2022-04-09 17:52:15 -0300

    .

--- app/src/main/java/com/example/gerenciadorgastos/MainActivity.java
+++ app/src/main/java/com/example/gerenciadorgastos/MainActivity.java
@@ -9,6 +9,7 @@ import android.widget.Toast;
 
 import java.util.HashMap;
 
+@SuppressWarnings("FieldCanBeLocal")
 public class MainActivity extends AppCompatActivity {
     private EditText txtGasto;
     private EditText txtValor;
