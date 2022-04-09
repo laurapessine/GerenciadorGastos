4a98aaa3ec60ad659594d61f797cfa95fd06660b
laurapessine <laurapessine@outlook.com>
2022-04-09 17:59:57 -0300

    .

--- app/src/main/java/com/example/gerenciadorgastos/MainActivity.java
+++ app/src/main/java/com/example/gerenciadorgastos/MainActivity.java
@@ -1,12 +1,10 @@
 package com.example.gerenciadorgastos;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
-
 import java.util.HashMap;
 
 @SuppressWarnings("FieldCanBeLocal")
@@ -28,10 +26,14 @@ public class MainActivity extends AppCompatActivity {
         btnLista = findViewById(R.id.btnLista);
         btnTotal = findViewById(R.id.btnTotal);
         btnAdicionar.setOnClickListener(view -> {
-            gastos.put(txtGasto.getText().toString(), Double.parseDouble(txtValor.getText().toString()));
-            Toast.makeText(getApplicationContext(), "Gasto adicionado!", Toast.LENGTH_LONG).show();
-            txtGasto.setText("");
-            txtValor.setText("");
+            if (txtGasto.getText().toString().isEmpty() || txtValor.getText().toString().isEmpty()) {
+                Toast.makeText(getApplicationContext(), "Nome ou valor não informado!", Toast.LENGTH_LONG).show();
+            } else {
+                gastos.put(txtGasto.getText().toString(), Double.parseDouble(txtValor.getText().toString()));
+                Toast.makeText(getApplicationContext(), "Gasto adicionado!", Toast.LENGTH_LONG).show();
+                txtGasto.setText("");
+                txtValor.setText("");
+            }
         });
         btnLista.setOnClickListener(view -> {
             for (String gasto : gastos.keySet()) {
